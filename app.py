2a16f972f5628ac3229c41269c54756ef2a65760
Ubuntu <bladezr2@JJAVM.lul50qoczfaupdkqdx4nu20v1c.bx.internal.cloudapp.net>
2021-11-07 04:04:28 +0000

    adding react and addng more functionality

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template
+import os
+import requests
+from dotenv import load_dotenv
+import flask
+import json
+
+
+load_dotenv()
+
+app = flask.Flask(__name__, static_folder="./build/static")
+bp = flask.Blueprint("bp", __name__, template_folder="./build")
+
+yelp_client_id = os.getenv("YELP_CLIENTID")
+yelp_api_key = os.getenv("YELP_APIKEY")
+business_search_url = "https://api.yelp.com/v3/businesses/search"
+newheaders = {'Authorization': 'bearer %s' % yelp_api_key}
+parameters = {'term':'food',
+    'location':'30303',
+    'limit': 25
+}
+search_response = requests.get(business_search_url, headers = newheaders, params = parameters)
+response_data = search_response.json()
+
+
+@bp.route('/index') 
+def index():
+    
+    return render_template('index.html')
+
+app.register_blueprint(bp)
+
+@app.route('/zipcode') 
+def zipcode():
+    return render_template('zipcode.html')
+
+@app.route('/zipcode', methods=['POST']) 
+def zipcode_post():
+
+    zip_code = flask.request.form.get("zip")
+    search_params = {'term':'restaurants',
+                     'location':zip_code,
+                     'limit':25
+     }
+    restaurant_search_response = requests.get(business_search_url, headers = newheaders, params = search_params)
+    restaurant_search_response_data = restaurant_search_response.json()
+    businesses =  restaurant_search_response_data["businesses"]
+
+    name = []
+    img_url = []
+    rating = []
+    is_closed = []
+    url = []
+    coord = []
+    id = []
+    
+    for business in businesses:
+        name.append(business["name"])
+        img_url.append(business["image_url"])
+        rating.append(business["rating"])
+        is_closed.append(business["is_closed"])
+        url.append(business["url"])
+        coord.append(business["coordinates"])
+        id.append(business["id"])
+
+
+    # print(name,img_url,rating,is_closed,url,coord,id)
+
+    DATA ={
+         "names":name,
+         "img_urls":img_url,
+         "ratings":rating,
+         "is_closeds":is_closed,
+         "urls":url,
+         "coords":coord,
+         "ids":id,
+    }
+    
+    data = json.dumps(DATA)
+    print(data)
+
+    return flask.render_template('index.html', data=data)
+
+@app.route('/') 
+def main():
+    return flask.redirect(flask.url_for("zipcode"))
+
+app.run(debug=True)
---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from "react";
+import './App.css';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
+import { useState, useRef } from 'react';
+
+const args = JSON.parse(document.getElementById("data").text);
+
+function Map(){
+
+    const[selectedRestaurant, setSelectedRestaurant] = useState(null);
+  
+    return(
+      <GoogleMap 
+          defaultZoom={10} 
+          defaultCenter={{lat:33.748997, lng:-84.387985}}
+      >
+          {args.coords.map((coordinates,index)=>(<Marker key={index} 
+          position={{
+              lat:coordinates["latitude"], 
+              lng:coordinates["longitude"]
+              }}
+              onClick = {()=>{
+                  setSelectedRestaurant(coordinates)
+              }}
+              />
+            ))}
+
+              {selectedRestaurant && (
+                  <InfoWindow
+                  position={{
+                    lat:selectedRestaurant["latitude"], 
+                    lng:selectedRestaurant["longitude"]
+                    }}
+                    onCloseClick={()=>{
+                        setSelectedRestaurant(null);
+
+                    }}
+                    ><div>Restaurant details</div></InfoWindow>
+              )}
+      </GoogleMap>
+    );
+  }
+
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+function App() {
+
+ 
+    
+    return (
+      <>
+      <h1>My Google Maps</h1>
+      {/* <div>
+      {args.names.map(name => (<p>{name}</p>))}
+      </div> */}
+      {/* <div>
+          {args.coords.map(coordinates =>(<p>lats:{coordinates["latitude"]}lng:{coordinates["longitude"]}</p>))}
+      </div> */}
+
+      <div style={{width:'100vw', height:'100vh'}}>
+      <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+        loadingElement={<div style={{height:"100%"}}/>}
+        containerElement={<div style={{height:"100%"}}/>}
+        mapElement={<div style={{height:"100%"}}/>}      
+    />
+    </div>
+      
+      </>            
+             
+    );
+  }
+  
+  export default App;
