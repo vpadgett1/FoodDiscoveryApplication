eec7ac532ea653e0ef025fec771df6a8d96b171d
Raneem Soufi <soufiraneem@gmail.com>
2021-11-12 16:35:14 -0500

    created merchant page

--- src/Pages/MerchantPage.js
+++ src/Pages/MerchantPage.js
@@ -1,15 +1,17 @@
 import '../App.css';
 import React, {
-  // useState,
+  useState,
   useEffect,
 } from 'react';
+import { useNavigate } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 // this line will become const MerchantPage = (props) => { once there are props
 const MerchantPage = () => {
   // set state
-  // const [state, setState] = useState(value);
+  const [currentSub, setCurrentSub] = useState('General');
 
+  const navigate = useNavigate();
   // deconstruct props
   // const [props] = props;
 
@@ -18,11 +20,118 @@ const MerchantPage = () => {
 
   }, []);
 
+  function selectSubcategory(choice) {
+    switch (choice) {
+      case 'General':
+        setCurrentSub('General');
+        break;
+      case 'YourPosts':
+        setCurrentSub('YourPosts');
+        break;
+      case 'CostumerPosts':
+        setCurrentSub('CostumerPosts');
+        break;
+      default:
+        setCurrentSub('General');
+        break;
+    }
+  }
+
+  const logout = async () => {
+    console.log('Logout');
+
+    // post to backend to log out usre
+
+    // go bck to main page
+    navigate('/');
+  };
+
+  function renderLeftSide() {
+    return (
+      <>
+        <button type="button" id="GeneralBtn" onClick={() => selectSubcategory('General')}>General</button>
+        <button type="button" id="YourPostsBtn" onClick={() => selectSubcategory('YourPosts')}>Your Posts</button>
+        <button type="button" id="CostumerPostsBtn" onClick={() => selectSubcategory('CostumerPosts')}>Costumer Posts</button>
+        <button type="button" id="LogoutBtn" onClick={logout}>Logout</button>
+      </>
+    );
+  }
+
+  function renderGeneral() {
+    return (
+      <>
+        <div>General</div>
+        <div>Restaurant Name: </div>
+        <div>Restaurant Yelp ID:</div>
+      </>
+    );
+  }
+
+  const onClickCreateNewPost = () => {
+    setCurrentSub('NewPost');
+  };
+
+  function renderYourPosts() {
+    return (
+      <>
+        <div>Your Posts</div>
+        <button type="button" onClick={onClickCreateNewPost}>New Post</button>
+      </>
+    );
+  }
+
+  function renderCostumerPosts() {
+    return (
+      <>
+        <div>Costumer Posts</div>
+      </>
+    );
+  }
+
+  const createNewPost = (event) => {
+    event.preventDefault();
+    setCurrentSub('YourPosts');
+  };
+
+  const backButtonPress = () => {
+    setCurrentSub('YourPosts');
+  };
+
+  function renderCreatePost() {
+    return (
+      <>
+        <button type="button" onClick={backButtonPress}>Back</button>
+        <div>Create Post</div>
+        <form onSubmit={createNewPost}>
+          <input type="text" placeholder="Enter Title" />
+          <input type="text" placeholder="Enter Body" />
+          <button type="submit">Publish</button>
+        </form>
+      </>
+    );
+  }
+
+  function renderBody() {
+    switch (currentSub) {
+      case 'General':
+        return renderGeneral();
+      case 'YourPosts':
+        return renderYourPosts();
+      case 'CostumerPosts':
+        return renderCostumerPosts();
+      case 'NewPost':
+        return renderCreatePost();
+      default:
+        return renderGeneral();
+    }
+  }
+
   // TODO: Render component
   return (
     <>
       <div>Merchant Page</div>
-      <div>This is the merchant page</div>
+      {renderLeftSide()}
+      {renderBody()}
     </>
   );
 };
