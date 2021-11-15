09c3d81f1f233d7b7bb3a35deaf06781eff3dc92
bseddiqi <bseddiqi@gmail.com>
2021-11-15 12:12:20 -0500

    creating items for each element of a post with HTML

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -7,6 +7,32 @@ import React, {
 
 // this line will become const Post = (props) => { once there are props
 const Post = () => {
+  const item = {
+    authorID: 'some-id',
+    postText: 'text',
+    postTitle:'Title',
+    postLikes: 'postLikes',
+    userID: 'userID',
+    postComments: 'postComments',
+    type: 'type',
+  };
+
+  return (
+    <div className = "post">
+      <h3> Username </h3>
+      <div className = "postTitle">
+        <h2>Post Title</h2>
+      </div>
+      <div className = "postText">
+        <p>Some information about some restaurant that user
+          has gone to and now talking about if friends should go
+          or not or if it really sucks.</p>
+          <div className = "postComment">
+            <p>Some comment someone left about the post</p>
+          </div>
+      </div>
+    </div>
+  )
   // set state
   // const [state, setState] = useState(value);
 
