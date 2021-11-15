0a57ac7d76ea5ab072f4fcb699fa589aacd38672
bseddiqi <bseddiqi@gmail.com>
2021-11-15 12:32:16 -0500

    Creating HTML to Discover page

--- src/Pages/DiscoverPage.js
+++ src/Pages/DiscoverPage.js
@@ -8,16 +8,21 @@ import Navigation from '../Components/Navigation';
 
 // this line will become const DiscoverPage = (props) => { once there are props
 const DiscoverPage = () => {
+  <Post/>;
   // set state
-  // const [state, setState] = useState(value);
+  const [items, setItems] = React.useState([{
+    type: null,
+    content: '',
+    id: '',
+  }]);
 
   // deconstruct props
   // const [props] = props;
 
-  // TODO: fetch data from backend
-  useEffect(() => {
+  // // TODO: fetch data from backend
+  // useEffect(() => {
 
-  }, []);
+  // }, []);
 
   // TODO: Render component
 
@@ -25,6 +30,21 @@ const DiscoverPage = () => {
     <>
       <Navigation />
       <div>This is the discover page</div>
+   
+      <div className = "post">
+      <h3> Username </h3>
+      <div className = "postTitle">
+        <h2>Post Title</h2>
+      </div>
+      <div className = "postText">
+        <p>Some information about some restaurant that user
+          has gone to and now talking about if friends should go
+          or not or if it really sucks.</p>
+          <div className = "postComment">
+            <p>Some comment someone left about the post</p>
+          </div>
+      </div>
+    </div>
     </>
   );
 };
