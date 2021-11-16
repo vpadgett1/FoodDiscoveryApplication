ced440d02f367491572b941263dcaf2dbec60966
Raneem Soufi <soufiraneem@gmail.com>
2021-11-16 17:34:57 -0500

    added additional test to make sure user page loads

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable no-undef */
 import {
   render, fireEvent, screen,
 } from '@testing-library/react';
-// import App from './App';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 import ProfilePage from './Pages/ProfilePage';
+import UserPage from './Pages/UserPage';
 
 // test that the body of the profile page changes as the user selects different subtabs
 test('Profile page subcategories render', () => {
@@ -54,5 +55,23 @@ test('Profile page subcategories render', () => {
   expect(GeneralSubcategory).toBeInTheDocument();
 });
 
-test('second test', () => {
+test('ensure a user page is loading all parts', () => {
+  const page = render(<Router><UserPage /></Router>);
+  expect(page.container.querySelector('#userPage')).toBeInTheDocument();
+
+  // user info has rendered
+  expect(page.container.querySelector('#basicInfo')).toBeInTheDocument();
+  expect(page.container.querySelector('#profilepic')).toBeInTheDocument();
+  expect(page.container.querySelector('#userID')).toBeInTheDocument();
+
+  // user favorite restaurants has rendered
+  expect(page.container.querySelector('#userFavRestaurants')).toBeInTheDocument();
+
+  // user posts has rendered
+  expect(page.container.querySelector('#userPosts')).toBeInTheDocument();
+
+  // if user presses the 'add friend' button, it should disable
+  const addFriendButton = page.container.querySelector('#addFriendButton');
+  fireEvent.click(addFriendButton);
+  expect(addFriendButton).toHaveAttribute('disabled');
 });
