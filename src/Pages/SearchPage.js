742e8cef9aaf1cfe7e578ca62c21792530e88f99
bseddiqi <bseddiqi@gmail.com>
2021-11-16 13:24:17 -0500

    created discover and search pageMVP

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -1,22 +1,21 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import '../App.css';
 import React, {
-// useState,
+  useState,
 // useEffect,
 } from 'react';
 import Navigation from '../Components/Navigation';
 // import PropTypes from 'prop-types';
 
-function searchBar() {
-  <form action="/" method="get">
-    <title>Search</title>
-    <label htmlFor="header-search">
-      <span className="visually-hidden"> Search restaurants</span>
-    </label>
-    <input id="searchbar" onKeyUp="searching()" type="text" name="search" placeholder="search restaurants..." />
-    <button type="submit">Search</button>
-  </form>;
-}
-
+const filterRestaurant = (posts, query) => {
+  if (!query) {
+    return posts;
+  }
+  return posts.filter((post) => {
+    const postName = post.name.toLowerCase();
+    return postName.includes(query);
+  });
+};
 const posts = [
   { id: '1', name: ' Pizza Restaurant' },
   { id: '2', name: ' Burger Restaurant' },
@@ -24,27 +23,67 @@ const posts = [
   { id: '4', name: ' Sushi Restaurant' },
   { id: '5', name: ' Poke Restaurant' },
 ];
-function searchPage() {
-  // set state
-  // const [state, setState] = useState(value);
+function SearchPage() {
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+  const filteredRestaurant = filterRestaurant(posts, query);
+  const [searchQuery, setSearchQuery] = useState(query || '');
 
-  // deconstruct props
-  // const [props] = props;
-
-  // TODO: fetch data from backend
-  useEffect(() => {
-
-  }, []);
-
-  // TODO: Render component
+  // search query to render restaurants that match search
   return (
     <>
       <Navigation />
       <div>This is the search page</div>
-    </>
 
+      <div>
+        <SearchPage
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        <ul>
+          {filteredRestaurant.map((post) => (
+            <li key={post.key}>{post.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
+
+// eslint-disable-next-line no-unused-vars
+const searchBar = ({ searchQuery, setSearchQuery }) => {
+  <form action="/" method="get">
+    <title>Search</title>
+    <label htmlFor="header-search">
+      <span className="visually-hidden">Search restaurants</span>
+    </label>
+    <input
+      value={searchQuery}
+      onInput={(e) => setSearchQuery(e.target.value)}
+      type="text"
+      id="header-search"
+      name="search"
+      placeholder="search restaurants..."
+    />
+    <button type="submit">Search</button>
+  </form>;
+};
+
+// set state
+// const [state, setState] = useState(value);
+
+// deconstruct props
+// const [props] = props;
+
+// TODO: fetch data from backend
+// useEffect(() => {
+
+// }, []);
+
+// TODO: Render component
+// return (
+
+// );
 
 // TODO: PropTypes
 SearchPage.propTypes = {
