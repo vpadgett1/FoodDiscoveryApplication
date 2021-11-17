943c5f1e71c75a4699368b0f9d9dd92b74c48277
bseddiqi <bseddiqi@gmail.com>
2021-11-16 21:50:55 -0500

    committing search changes

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,45 +6,45 @@ import React, {
 } from 'react';
 // import Navigation from './Navigation';
 // import PropTypes from 'prop-types';
-function Search() {
-  // search query to render restaurants that match search
 
-  // eslint-disable-next-line no-unused-vars
-  const searchBar = ({ searchQuery, setSearchQuery }) => {
-    <form action="/" method="get">
-      <title>Search</title>
-      <label htmlFor="header-search">
-        <span className="visually-hidden">Search restaurants</span>
-      </label>
-      <input
-        value={searchQuery}
-        onInput={(e) => setSearchQuery(e.target.value)}
-        type="text"
-        id="header-search"
-        name="search"
-        placeholder="search restaurants..."
-      />
-      <button type="submit">Search</button>
-    </form>;
-  };
+// search query to render restaurants that match search
 
-  // set state
-  // const [state, setState] = useState(value);
+// eslint-disable-next-line no-unused-vars
+const Search = ({ searchQuery, setSearchQuery }) => {
+  <form action="/" method="get">
+    <title>Search</title>
+    <label htmlFor="header-search">
+      <span>Search restaurants</span>
+    </label>
+    <input
+      value={searchQuery}
+      onInput={(e) => setSearchQuery(e.target.value)}
+      type="text"
+      id="header-search"
+      name="s"
+      placeholder="search restaurants..."
+    />
+    <button type="submit">Search</button>
+  </form>;
+};
 
-  // deconstruct props
-  // const [props] = props;
+// set state
+// const [state, setState] = useState(value);
 
-  // TODO: fetch data from backend
-  // useEffect(() => {
+// deconstruct props
+// const [props] = props;
 
-  // }, []);
+// TODO: fetch data from backend
+// useEffect(() => {
 
-  // TODO: Render component
-  // return (
+// }, []);
 
-  // );
+// TODO: Render component
+// return (
+
+// );
 
 // TODO: PropTypes
 // SearchPage.propTypes = {
-}
+
 export default Search;
