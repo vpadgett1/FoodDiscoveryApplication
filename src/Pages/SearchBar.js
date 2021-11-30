b540e607833681b7b27ed973af548a26d3b02696
bseddiqi <bseddiqi@gmail.com>
2021-11-30 01:14:42 -0500

    added specifics for search bar

--- src/Pages/SearchBar.js
+++ src/Pages/SearchBar.js
@@ -4,7 +4,11 @@ const SearchBar = ({keyword, setKeyword}) => {
     const BarStyling = {width:"20rem", background:"#F2f1f9", border:"none", padding = "0.5rem"};
     return(
         <input
-
+        style = {BarStyling}
+        key = "random1"
+        value={keyword}
+        placeholder = {'search'}
+        onChange = {(e) => setKeyword(e.target.value)}
         />
     );
 }
