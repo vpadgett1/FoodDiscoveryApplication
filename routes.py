fbe3eb24d2a5420d680718bce8941ecf16f80a7e
vpadgett1 <vpadgett1@student.gsu.edu>
2021-11-30 15:34:31 -0500

    updated the return value so heroku doesn't yell at me

--- routes.py
+++ routes.py
@@ -916,5 +916,5 @@ def main():
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "127.0.0.1"), port=int(os.getenv("PORT", 5000)), debug=True
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 5000)), debug=True
     )
